834a3c738097726d8a97a1b35012b512cb3c78ae
engenhariacct <119813718+engenhariacct@users.noreply.github.com>
2023-04-04 16:47:53 -0300

    Update checkinFABLAB.py

--- checkinFABLAB.py
+++ checkinFABLAB.py
@@ -46,10 +46,11 @@ with col1:
     #st.image(https://github.com/engenhariacct/webappCHECKin/blob/main/LOGO%20-%20FabLLab.JPG, width=200, output_format="auto")
     st.image('Selo20anosCCT.PNG', width=200, output_format='auto')  
 with col2:    
-    st.image('LOGO - FabLLab.JPG', width=200, output_format='auto')
+    st.image('LOGO - FabLLab.JPG', width=150, output_format='auto')
 with col3:
-    st.title("PAINEL - FabLab")
-    st.header("Mensagens dos visitantes:")
+    st.title("PAINEL")
+    st.title("FabLab")
+    st.header("Mensagens:")
 st.pyplot() #Este método faz exibirt a nuvem de palavras
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.video("https://www.youtube.com/watch?v=IYJKM3ie9sE&list=PLMQP5Jy3lKrMVgnuGfCCldqOjo_lGksM4")

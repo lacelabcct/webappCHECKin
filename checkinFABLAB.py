896d6f538434875a2b7fb64d404f8fa139bc1bb5
engenhariacct <119813718+engenhariacct@users.noreply.github.com>
2023-04-04 16:56:07 -0300

    Update checkinFABLAB.py

--- checkinFABLAB.py
+++ checkinFABLAB.py
@@ -53,4 +53,5 @@ wordcloud.to_file("Mensagens_dos_Visitantes.png")
 
 st.pyplot() #Este método faz exibirt a nuvem de palavras
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.info("Desenvolvido em Linguagem Python. Equipe FabLab / Prof. Massaki de O. Igarashi")
+st.info("Desenvolvido em Linguagem Python.")
+st.info("Equipe FabLab/Programado pelo Prof. Massaki de O. Igarashi")
